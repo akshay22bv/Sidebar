750b1276ab6e19036616a70cbd712f5652f178b9
akshay22bv <akshay22bv@gmail.com>
2022-11-16 22:10:02 +0530

    navbar changes

--- src/components/NavPage.js
+++ src/components/NavPage.js
@@ -1,29 +1,16 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Angular from "../pages/Angular";
-import Css from "../pages/Css";
-import Figma from "../pages/Figma";
-import Git from "../pages/Git";
-import Home from "../pages/Home";
-import Java from "../pages/Java";
-import Node from "../pages/Node";
-import Php from "../pages/Php";
-import PageReact from "../pages/PageReact";
+
+import { SidebarData } from "../data/SidebarData";
 
 const NavPage = () => {
   return (
     <React.Fragment>
       <section>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/java" element={<Java />} />
-          <Route path="/css" element={<Css />} />
-          <Route path="/php" element={<Php />} />
-          <Route path="/figma" element={<Figma />} />
-          <Route path="/git" element={<Git />} />
-          <Route path="/node" element={<Node />} />
-          <Route path="/react" element={<PageReact />} />
-          <Route path="/angular" element={<Angular />} />
+          {SidebarData.map((item) => (
+            <Route key={item.path} path={item.path} element={item.element} />
+          ))}
         </Routes>
       </section>
     </React.Fragment>
